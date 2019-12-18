2a6d3b4d736f171893c536e53a4ce6f003355d32
Sean Nealon <signalflowsean@gmail.com>
2019-12-17 19:42:11 -0500

    Hooked up s3 env variables to docker-compose.yml file. Created a file where one can create an s3 bucket, upload files to it, download files from it, and delete the bucket. Also, added an example route to print files from a bucket for testing purposes

--- api/routes/deeplearner/route.py
+++ api/routes/deeplearner/route.py
@@ -1,11 +1,13 @@
 # ---- THIRD PARTY IMPORTS ----
+import os
+
 # pylint: disable=F0401
 from flask import Blueprint
 from flask import request
 
 # ---- FIRST PARTY IMPORTS ----
 # pylint: disable=E0611
-from utils import umap
+from utils import umap, s3
 
 deeplearner_blueprint = Blueprint('deeplearner', __name__)
 
@@ -14,6 +16,14 @@ deeplearner_blueprint = Blueprint('deeplearner', __name__)
 @deeplearner_blueprint.route('/')
 def testConnection():
   return umap.test()
+
+# Hit this endpoint to add audio library
+# Currently, only prints out the name of files in a bucket
+@deeplearner_blueprint.route('/audiolibrary')
+def printAudioLibraryInfo(): 
+  # A bucketname of a bucket on s3
+  bucketname = 'audio-library-02ee2898-212d-11ea-9a8c-0242c0a85002'
+  return '\n'.join(s3.getAllFilesNamesInABucket(bucketname))
 
 # Hit this endpoint to compute umap features
 @deeplearner_blueprint.route('/features', methods = ['POST', 'GET'])
